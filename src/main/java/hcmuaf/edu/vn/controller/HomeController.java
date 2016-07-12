893590819bea3f0953e07e0a56b6d2057231ce26
Pham Phu Minh Quan <mquan2793@gmail.com>
2016-07-13 00:49:27 +0700

    hello

--- src/main/java/hcmuaf/edu/vn/controller/HomeController.java
+++ src/main/java/hcmuaf/edu/vn/controller/HomeController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "/home")
+@RequestMapping(value = "/home") 
 public class HomeController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String direction() {
 
-		System.out.println("Home controller");
+		System.out.println("Home hello controller");
 		// return "redirect:/organisation/addOrg";
 		// return "redirect:/organisation/layout";
 		return "redirect:/home";
